2ff4118ea598e0dd73df09f5830f4f6a95ab831e
Git- Rambodevv <103598800+Gitsimplon@users.noreply.github.com>
2023-02-19 23:53:32 +0100

    Update treeConstructor.js

---
+++ rush01/treeConstructor.js
@@ -0,0 +1,32 @@
+
+
+function treeConstructor(strArr) {
+  const parents = {};
+  const children = {};
+
+  for (const pair of strArr) {
+    const [child, parent] = pair.replace(/[()]/g, '').split(',');
+
+    if (parents[parent]) {
+      parents[parent].push(child);
+      if (parents[parent].length > 2) {
+        return false;
+      }
+    } else {
+      parents[parent] = [child];
+    }
+
+    if (children[child]) {
+      return false;
+    } else {
+      children[child] = parent;
+    }
+  }
+
+  return true;
+}
+
+console.log(TreeConstructor(["(1,2)", "(2,4)", "(5,7)", "(7,2)", "(9,5)"]));
+console.log(TreeConstructor(["(1,2)", "(3,2)", "(2,12)", "(5,2)"]));
+
+module.exports = treeConstructor;
